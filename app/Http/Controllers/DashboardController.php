a899327a91ab2afcdbe4f29cf97f9cf61c088a00
Michael <michaelfarquhar4@gmail.com>
2022-06-12 14:16:46 -0400

    Show proper questions & answers count on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,10 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $questions_count = auth()->user()->questions->count();
+        $answers_count = auth()->user()->answers->count();
+        $upvotes_count = 0;
+        $solutions_count = 0;
+        return view('dashboard.index', compact('questions_count', 'answers_count', 'upvotes_count', 'solutions_count'));
     }
 }
--- resources/views/dashboard/stats.blade.php
+++ resources/views/dashboard/stats.blade.php
@@ -1,18 +1,18 @@
 <div class="w-full flex justify-evenly space-x-16">
     <div class="flex-1 py-5 px-10 bg-gray-100 rounded-3xl text-center">
         <div class="uppercase text-sm font-bold text-slate-500">Questions</div>
-        <div class="text-3xl mt-2 text-slate-600">12</div>
+        <div class="text-3xl mt-2 text-slate-600">{{$questions_count}}</div>
     </div>
     <div class="flex-1 py-5 px-10 bg-gray-100 rounded-3xl text-center">
         <div class="uppercase text-sm font-bold text-slate-500">Answers</div>
-        <div class="text-3xl mt-2 text-slate-600">4</div>
+        <div class="text-3xl mt-2 text-slate-600">{{$answers_count}}</div>
     </div>
     <div class="flex-1 py-5 px-10 bg-gray-100 rounded-3xl text-center">
         <div class="uppercase text-sm font-bold text-slate-500">Upvotes</div>
-        <div class="text-3xl mt-2 text-slate-600">30</div>
+        <div class="text-3xl mt-2 text-slate-600">{{$upvotes_count}}</div>
     </div>
     <div class="flex-1 py-5 px-10 bg-gray-100 rounded-3xl text-center">
         <div class="uppercase text-sm font-bold text-slate-500">Solutions</div>
-        <div class="text-3xl mt-2 text-slate-600">2</div>
+        <div class="text-3xl mt-2 text-slate-600">{{$solutions_count}}</div>
     </div>
 </div>
